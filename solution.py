e7b755ab684901aebdb26e838510b5844095a180
ws2309nyu <95750304+ws2309nyu@users.noreply.github.com>
2021-12-08 06:56:08 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -20,13 +20,13 @@ def welcome_assignment_answers(question):
                      "hash generator and use the answer in your code":
         answer = "42b76fe51778764973077a5a94056724"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a " \
                      "numeric number":
-        answer = '5'
+        answer = "5"
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric " \
                      "number":
-        answer = '4'
+        answer = "4"
     return (answer)
 
 
