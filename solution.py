544044d5684e79171fc7fb7c46fba6b697128fc3
ws2309nyu <95750304+ws2309nyu@users.noreply.github.com>
2021-12-08 06:52:51 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,11 +7,11 @@ def welcome_assignment_answers(question):
     # Another way to implement is using a "case" statements similar to C.
     if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 " \
                    "channel posted by a TA?":
-        answer = ""
+        answer = "mtls"
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
@@ -23,10 +23,10 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a " \
                      "numeric number":
-        answer = "4"
+        answer = "5"
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric " \
                      "number":
-        answer = "3"
+        answer = "4"
     return (answer)
 
 
