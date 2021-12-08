55ee59a3a8235a72088c363d986c66291a6159b3
ws2309nyu <95750304+ws2309nyu@users.noreply.github.com>
2021-12-08 06:54:35 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,7 +11,7 @@ def welcome_assignment_answers(question):
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
@@ -20,13 +20,13 @@ def welcome_assignment_answers(question):
                      "hash generator and use the answer in your code":
         answer = "42b76fe51778764973077a5a94056724"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a " \
                      "numeric number":
-        answer = "5"
+        answer = '5'
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric " \
                      "number":
-        answer = "4"
+        answer = '4'
     return (answer)
 
 
