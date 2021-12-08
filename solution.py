42ab9a2e2bf0545febbafda0463808d4afa5013a
ws2309nyu <95750304+ws2309nyu@users.noreply.github.com>
2021-12-08 06:59:58 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,11 +11,11 @@ def welcome_assignment_answers(question):
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 " \
                      "hash generator and use the answer in your code":
         answer = "42b76fe51778764973077a5a94056724"
